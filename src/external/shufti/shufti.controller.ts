5e898c4f99f57f12d0468e4fb2893f498bcfa6e5
Murtuza Ranapurwala <murtuza.ranapurwala@gojo.co>
2022-08-17 14:49:00 +0530

    Adds Logs

--- src/external/shufti/shufti.controller.ts
+++ src/external/shufti/shufti.controller.ts
@@ -1,13 +1,16 @@
 import {Body, Controller, Param, Post, Query} from "@nestjs/common";
 import {ShuftiResponseDto} from "./dto/shufti-response.dto";
 import {ShuftiService} from "./shufti.service";
+import {CustomLogger} from "../../custom_logger";
 
 @Controller('shufti')
 export class ShuftiController{
+    private readonly logger = new CustomLogger(ShuftiController.name);
     constructor(private readonly shuftiService: ShuftiService) {}
 
     @Post('callback')
     async callback(@Body() response: ShuftiResponseDto, @Query() params: any){
+        this.logger.log(`Received callback from shufti ${JSON.stringify(response)} with params ${JSON.stringify(params)}`)
         await this.shuftiService.kycCallback(params.dreamerId, params.kycId, response);
     }
 }
