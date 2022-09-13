f5100df6a1159d5b0b7264720043758f9316985a
Nitesh_soni <nitesh@readybytes.in>
2022-09-13 12:10:04 +0530

    Client Module: added service with get & post request

---
+++ src/modules/loan_management/client/dto/create-client.dto.ts
@@ -0,0 +1,18 @@
+export class CreateClientDto {
+  id: string;
+  zoho_id: string;
+  sendpulse_id: string;
+  first: string;
+  last: string;
+  full_en: string;
+  nickname: string;
+  dob: Date;
+  mobile: string;
+  acc_provider_type: string;
+  acc_number: string;
+  acc_note: string;
+  summary_loanCycle: number;
+  summary_has_loan_in_arrear: string;
+  summary_membership_level: string;
+  acc_update_date: string;
+}
