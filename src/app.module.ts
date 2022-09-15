561221e1a8683def5b958f7f5854fbc79078b608
Mohit Joshi <mohitjoshi@Mohits-MacBook-Pro.local>
2022-09-15 12:59:50 +0530

    Loan Management Initial commit: create Client and Loan APIs

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,20 @@
-import {Module} from '@nestjs/common';
-import {ConfigModule} from '@nestjs/config';
-import {ZohoModule} from './external/zoho/zoho.module';
-import {DreamerModule} from "./dreamer/dreamer.module";
-import {SendpulseModule} from "./external/sendpulse/sendpulse.module";
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { ZohoModule } from './external/zoho/zoho.module';
+import { DreamerModule } from "./dreamer/dreamer.module";
+import { SendpulseModule } from "./external/sendpulse/sendpulse.module";
 import { EventEmitterModule } from '@nestjs/event-emitter';
-import {HealthCheckController} from "./config/health-check.controller";
+import { HealthCheckController } from "./config/health-check.controller";
+import { TypeOrmModule } from '@nestjs/typeorm';
+import DatabaseConfig from './config/database.config';
+import { ClientModule } from "./loan_management/client/client.module";
+import { LoanModule } from "./loan_management/loan/loan.module";
 
 @Module({
   controllers: [HealthCheckController],
-  imports: [ConfigModule.forRoot(), EventEmitterModule.forRoot(), ZohoModule, DreamerModule, SendpulseModule]
+  imports: [
+    TypeOrmModule.forRoot(DatabaseConfig),
+    ConfigModule.forRoot(), EventEmitterModule.forRoot(), ZohoModule, DreamerModule, SendpulseModule, ClientModule, LoanModule]
 })
 export class AppModule {
 
