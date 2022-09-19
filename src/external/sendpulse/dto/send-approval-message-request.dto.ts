081447ffbf179e81171dca11541fdf7f7ea2b8a6
Mohit Joshi <mohitjoshi@Mohits-MacBook-Pro.local>
2022-09-19 15:10:59 +0530

    Support for WireTransferType in API

--- src/external/sendpulse/dto/send-approval-message-request.dto.ts
+++
@@ -1,7 +0,0 @@
-export class SendApprovalMessageRequestDto {
-sendpulse_user_id: string;
-message: string;
-loan_amount: number;
-application_status: string;
-}
-
--- src/external/sendpulse/sendpulse.controller.ts
+++ src/external/sendpulse/sendpulse.controller.ts
@@ -4,18 +4,20 @@ import { SendMessageRequestDto } from './dto/send-message-request.dto';
 import { RunFlowRequestDto } from './dto/run-flow-request.dto';
 import {CustomLogger} from "../../custom_logger";
 import { DreamerModel } from '../../dreamer/usecases/model/dreamer.model';
-import { SendApprovalMessageRequestDto } from './dto/send-approval-message-request.dto';
+import { UpdateApplicationStatusRequestDto } from './dto/update-application-status-request.dto';
 import { SetVariableRequestDto } from './dto/set-variable-request.dto';
 import { CalculateLoanDto } from './dto/calculate-loan.dto';
 import { CalculationDto } from './dto/calculation.dto';
 import { SendpulseHelperService } from './sendpulse-helper.service';
 import { CalculationResultDto } from './dto/calculation-result.dto';
+import { id } from 'date-fns/locale';
 
 @Controller('sendpulse')
 export class SendpulseController {
 
   private readonly log = new CustomLogger(SendpluseService.name);
   private readonly SENDPULSE_MESSAGING_FLOWID="62fc9cd35c6b0b21d713cdea";
+  private readonly APPLICATION_STATUS = ["Approved", "Not Qualified", "Disbursed"]
 
   constructor(
     private readonly sendpulseService: SendpluseService,    
@@ -48,7 +50,7 @@ export class SendpulseController {
   }
 
   @Post('/updateApplicationStatus')
-  async updateApplicationStatus(@Body() reqData: SendApprovalMessageRequestDto ) {
+  async updateApplicationStatus(@Body() reqData: UpdateApplicationStatusRequestDto ) {
     this.log.log(JSON.stringify(reqData));
 
     let appStatusDto = new SetVariableRequestDto();
@@ -65,6 +67,15 @@ export class SendpulseController {
     amountDto.variable_value= ""+ reqData.loan_amount;
     await this.sendpulseService.setVariable(amountDto);
 
+    if(reqData.application_status === this.APPLICATION_STATUS[2]){
+      let transfertypeDto = new SetVariableRequestDto();
+      transfertypeDto.contact_id=reqData.sendpulse_user_id;
+      transfertypeDto.variable_name="wingTransferType";
+      transfertypeDto.variable_id="632810d223589356b87d9195";
+      transfertypeDto.variable_value= ""+ reqData.wire_transfer_type;
+      await this.sendpulseService.setVariable(transfertypeDto);
+    }
+    
     let model = new DreamerModel();
     model.externalId=reqData.sendpulse_user_id;
     model.external_data = {};
@@ -72,35 +83,5 @@ export class SendpulseController {
     await this.sendpulseService.runFlow(model, this.SENDPULSE_MESSAGING_FLOWID) ;
     return {"status": 'ok'}
   }
-
-  @Post('/sendApprovalMessage')
-  async sendApprovalMessage(@Body() reqData: SendApprovalMessageRequestDto ) {
-    this.log.log(JSON.stringify(reqData));
-
-    let appStatusDto = new SetVariableRequestDto();
-    appStatusDto.contact_id=reqData.sendpulse_user_id;
-    appStatusDto.variable_name="application_status";
-    appStatusDto.variable_id="6319a9390219f75deb1c07d3";
-    appStatusDto.variable_value= reqData.application_status;
-    await this.sendpulseService.setVariable(appStatusDto);
-
-    let amountDto = new SetVariableRequestDto();
-    amountDto.contact_id=reqData.sendpulse_user_id;
-    amountDto.variable_name="approved_rejected_amount";
-    amountDto.variable_id="6319aa4720f4c45a1b390826";
-    amountDto.variable_value= ""+ reqData.loan_amount;
-    await this.sendpulseService.setVariable(amountDto);
-
-    let model = new DreamerModel();
-    model.externalId=reqData.sendpulse_user_id;
-
-    //TODO: Language Support
-    let approval_message = "There have been some issue with loan request. We will contact you soon.";
-    model.external_data = {"message": approval_message};
-
-    await this.sendpulseService.runFlow(model, this.SENDPULSE_MESSAGING_FLOWID) ;
-    return {"status": 'ok'}
-  }
-
 
 }
