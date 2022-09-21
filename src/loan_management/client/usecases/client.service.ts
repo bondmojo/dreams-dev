129658987d3ee41f4bc783f00031bf201bc7713f
Nitesh_soni <nitesh@readybytes.in>
2022-09-21 15:29:37 +0530

    client: added client update function and event

--- src/loan_management/client/usecases/client.service.ts
+++ src/loan_management/client/usecases/client.service.ts
@@ -1,14 +1,15 @@
 import { Injectable } from "@nestjs/common";
 import { InjectRepository } from '@nestjs/typeorm';
-import { CreateClientAndLoanDto, CreateClientDto, GetClientDto } from "../dto";
+import { CreateClientAndLoanDto, CreateClientDto, GetClientDto, UpdateClientDto } from "../dto";
 import { CustomLogger } from "../../../custom_logger";
 import { Client } from '../entities/client.entity';
 import { Repository } from 'typeorm';
 import { LoanService } from "../../loan/usecases/loan.service";
+import { OnEvent } from "@nestjs/event-emitter";
 
 @Injectable()
 export class ClientService {
-    private readonly log = new CustomLogger(ClientService.name);
+    private readonly logger = new CustomLogger(ClientService.name);
     constructor(
         @InjectRepository(Client)
         private readonly clientRepository: Repository<Client>,
@@ -16,7 +17,6 @@ export class ClientService {
     ) { }
 
     async create(createClientAndLoanDto: CreateClientAndLoanDto): Promise<Client> {
-
         // Generating client id
         const createClientDto: CreateClientDto = createClientAndLoanDto;
         createClientDto.id = createClientAndLoanDto.client_id = 'CL' + Math.floor(Math.random() * 100000000);
@@ -32,6 +32,12 @@ export class ClientService {
             where: fields,
         });
         return client;
+    }
+
+    @OnEvent('client.update')
+    async update(updateClientDto: UpdateClientDto) {
+        this.logger.log(`Updating client with data ${JSON.stringify(updateClientDto)}`);
+        await this.clientRepository.update(updateClientDto.id, updateClientDto);
     }
 
 }
