0d5fb9321f23eb448043e1b0e76671e16ceaaf2d
Mohit Joshi <mohit.joshi@gojo.co>
2022-10-10 17:30:06 +0530

    fixed API Name fileUpload

--- src/files/file_upload.controller.ts
+++ src/files/file_upload.controller.ts
@@ -4,13 +4,19 @@ import { FileInterceptor } from '@nestjs/platform-express';
 import { Util } from 'src/config/util';
 @Controller('files')
 export class FileUploadController {
+
+  private readonly S3_PAYMENT_FOLDER = "PaymentRepceipts/";
+  private readonly S3_PAYMENT_FILE_TYPE = ".jpg";
+
   constructor(private readonly fileUploadService: FileUploadService) { }
-  @Post('/upload')
+
+
+  @Post('uploadPaymentReceipt')
   @UseInterceptors(FileInterceptor('file'))
-  async uploadFile(@UploadedFile() file: Express.Multer.File) {
+  async uploadPaymentReceipt(@UploadedFile() file: Express.Multer.File) {
     try {
       const key = Util.getRandomAlphaNumericString(10, 'a');
-      const upload = await this.fileUploadService.upload(key, file);
+      const upload = await this.fileUploadService.upload(this.S3_PAYMENT_FOLDER + key + this.S3_PAYMENT_FILE_TYPE, file);
       console.log(upload);
       return key;
     } catch (err) {
