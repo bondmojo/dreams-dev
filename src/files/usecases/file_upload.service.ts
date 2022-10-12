5d7fca6a915ec9d62f2bb330fcc2173fe473729c
Mohit Joshi <mohit.joshi@gojo.co>
2022-10-12 17:12:59 +0530

    1. Created RunFlow API 2.changes in initiateKyc :Shufti flow. Updated redirected URL too 3. added env support to send file to s3

--- src/files/usecases/file_upload.service.ts
+++ src/files/usecases/file_upload.service.ts
@@ -25,27 +25,33 @@ export class FileUploadService {
         file: Express.Multer.File
     ): Promise<PutObjectCommandOutput> {
 
-        this.logger.log("UPLOAD File. STS Client Request Params =" + JSON.stringify(this.params));
-        /* const data = await this.stsClient.send(new AssumeRoleCommand(this.params));
-        this.logger.log("STS Client response =" + JSON.stringify(data));
+        this.logger.log("Uploading File.");
+        let s3ClientParams;
+        if (process.env.NODE_ENV == 'local') {
+            this.logger.log("STS Client Request Params =" + JSON.stringify(this.params));
+            const data = await this.stsClient.send(new AssumeRoleCommand(this.params));
+            this.logger.log("STS Client response =" + JSON.stringify(data));
 
-        const rolecreds = {
-            accessKeyId: data.Credentials?.AccessKeyId,
-            secretAccessKey: data.Credentials?.SecretAccessKey,
-            sessionToken: data.Credentials?.SessionToken,
-        };
-        const s3Client = new S3({
-            credentials: {
-                accessKeyId: rolecreds.accessKeyId ?? 'AKIA6L3THESLIAJO6RQ7',
-                secretAccessKey: rolecreds.secretAccessKey ?? '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',
-                sessionToken: rolecreds.sessionToken
-            },
-            region: process.env.SK_S3_REGION,
-        }); */
+            const rolecreds = {
+                accessKeyId: data.Credentials?.AccessKeyId,
+                secretAccessKey: data.Credentials?.SecretAccessKey,
+                sessionToken: data.Credentials?.SessionToken,
+            };
 
-        const s3Client = new S3({
-            region: process.env.SK_S3_REGION
-        });
+            s3ClientParams = {
+                credentials: {
+                    accessKeyId: rolecreds.accessKeyId ?? 'AKIA6L3THESLIAJO6RQ7',
+                    secretAccessKey: rolecreds.secretAccessKey ?? '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',
+                    sessionToken: rolecreds.sessionToken
+                },
+                region: process.env.SK_S3_REGION,
+            };
+        }
+        else {
+            s3ClientParams = { region: process.env.SK_S3_REGION }
+        }
+
+        const s3Client = new S3(s3ClientParams);
 
         const bucketParams = {
             Bucket: process.env.SK_S3_SYSTEM_DATA_BUCKET,
@@ -54,8 +60,6 @@ export class FileUploadService {
             ACL: 'public-read',
             ContentType: file.mimetype,
         };
-        //return await s3Client.send(new PutObjectCommand(bucketParams));
-        //let decodedImage = Buffer.from(encodedImage, 'base64');
 
         return await s3Client.putObject(bucketParams);
     }
