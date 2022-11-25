ac9b12210f9e67ec22ae5e7e96be84b8e72f6c31
Nitesh_soni <nitesh@readybytes.in>
2022-11-25 16:12:25 +0530

    Fix house_no migration

--- src/migrations/1669277639274-AddHouseNoInClientTable.ts
+++ src/migrations/1669277639274-AddHouseNoInClientTable.ts
@@ -4,15 +4,15 @@ export class AddHouseNoInClientTable1669277639274 implements MigrationInterface 
     name = 'AddHouseNoInClientTable1669277639274'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE \`clients\` CHANGE \`updated_date\` \`house_no\` timestamp NOT NULL`);
-        await queryRunner.query(`ALTER TABLE \`clients\` DROP COLUMN \`house_no\``);
+        // await queryRunner.query(`ALTER TABLE \`clients\` CHANGE \`updated_date\` \`house_no\` timestamp NOT NULL`);
+        // await queryRunner.query(`ALTER TABLE \`clients\` DROP COLUMN \`house_no\``);
         await queryRunner.query(`ALTER TABLE \`clients\` ADD \`house_no\` varchar(255) NULL`);
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`ALTER TABLE \`clients\` DROP COLUMN \`house_no\``);
-        await queryRunner.query(`ALTER TABLE \`clients\` ADD \`house_no\` timestamp NOT NULL`);
-        await queryRunner.query(`ALTER TABLE \`clients\` CHANGE \`house_no\` \`updated_date\` timestamp NOT NULL`);
+        // await queryRunner.query(`ALTER TABLE \`clients\` ADD \`house_no\` timestamp NOT NULL`);
+        // await queryRunner.query(`ALTER TABLE \`clients\` CHANGE \`house_no\` \`updated_date\` timestamp NOT NULL`);
     }
 
 }
