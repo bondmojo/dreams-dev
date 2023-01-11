9b22f5cb5306bf8b841da69de6c90bfd0d31c4b3
Nitesh_soni <nitesh@readybytes.in>
2023-01-11 13:45:11 +0530

    Added zoho field mapping file for instalment

---
+++ src/globals/zoho_fields_mapping/Installment.ts
@@ -0,0 +1,11 @@
+export const ZohoInstallmentFields = {
+    name: 'Name',
+    loan_id: 'Loan_Id',
+    repayment_date: 'Repayment_Date',
+    last_paid_date: 'Last_Paid_Date',
+    overdue_amount: 'Overdue_Amount',
+    repayment_status: 'Repayment_Status',
+    total_paid_amount: 'Total_Paid_Amount',
+    due_principal_amount: 'Due_Principal_Amount',
+    installment_status: 'Installment_Status',
+}
