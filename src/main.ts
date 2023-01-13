c250906cc9564aa39e5cd21458e0f623b7fe8479
Nitesh_soni <nitesh@readybytes.in>
2023-01-13 10:22:09 +0530

    Handle unhandled rejection & uncaught exception

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-
+import './error/global-error.handler';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
