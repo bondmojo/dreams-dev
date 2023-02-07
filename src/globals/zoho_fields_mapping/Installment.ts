b03b049dd0440d15867c1b5336cfc4f6ba5790ed
Nitesh_soni <nitesh@readybytes.in>
2023-02-07 15:11:41 +0530

    Add fields in instalment zoho model

--- src/globals/zoho_fields_mapping/Installment.ts
+++ src/globals/zoho_fields_mapping/Installment.ts
@@ -5,9 +5,10 @@ export const ZohoInstallmentFields = {
     last_paid_date: 'Last_Paid_Date',
     overdue_amount: 'Overdue_Amount',
     repayment_status: 'Repayment_Status',
-    total_paid_amount: 'Total_Paid_Amount',
+    total_paid_amount: 'Paid_Amount',
     principal_amount: 'Principal_Amount',
     installment_status: 'Installment_Status',
     installment_fees: 'Installment_Fees',
-    loan_completion_date: 'Loan_completion_Date'
+    loan_completion_date: 'Loan_completion_Date',
+    additional_fee: 'Additional_Amount'
 }
