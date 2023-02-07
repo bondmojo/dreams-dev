ca0bb38787e03a1d4eddfceca81037af2f12632a
Mohit Joshi <mohit.joshi@gojo.co>
2023-02-07 15:10:09 +0530

    1. Language support added 2. Repayment schedule message API

---
+++ src/external/sendpulse/dto/calculate-repayment-schedule.dto.ts
@@ -0,0 +1,7 @@
+export default class CalculateRepaymentScheduleDto {
+  loan_amount: string;
+  loan_tenure: string;
+  loan_type: string;
+  wing_wei_luy_transfer_fee: string;
+  currency: string;
+}
